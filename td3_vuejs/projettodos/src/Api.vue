8a5ac73c3270e1e24a34824f3614b962a34a0da1
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2024-03-27 18:05:56 +0100

    commencement du projet

---
+++ td3_vuejs/projettodos/src/Api.vue
@@ -0,0 +1,7 @@
+<script>
+export async function recupererQuestionnaires() {
+  const response = await fetch('http://127.0.0.1:5000/questionnaires');
+  const data = await response.json();
+  return data;
+}
+</script>
--- td3_vuejs/projettodos/src/App.vue
+++ td3_vuejs/projettodos/src/App.vue
@@ -1,40 +1,90 @@
 <template>
-  <div>
-    <h1>Liste des tâches</h1>
+  <div class="app">
+    <h1>Liste des questionnaires</h1>
     <ul>
-      <TodoItem v-for="task in tasks" :key="task.id" :task="task" @remove="removeItem" @modifier="modifierItem"/>
+      <QuestionnaireItem 
+        v-for="questionnaire in questionnaires" 
+        :key="questionnaire.id" 
+        :questionnaire="questionnaire" 
+        @afficherDetail="afficherDetail"
+      />
     </ul>
+  </div>
+  <div class="detail">
   </div>
 </template>
 
 <script setup>
-import TodoItem from './components/TodoItem.vue';
+import QuestionnaireItem from './components/QuestionnaireItem.vue';
 import { ref, onMounted } from 'vue';
+import * as api from './Api.vue';
 
-const tasks = ref([]);
+const questionnaires = ref([]);
+const detailContent = ref('');
 
 onMounted(async () => {
-  const response = await fetch('http://localhost:5000/todo/api/v1.0/tasks');
-  const data = await response.json();
-  tasks.value = data.tasks.map(task => ({ id: task.id, title: task.description }));
+  try {
+    const data = await api.recupererQuestionnaires();
+    console.log(data.questionnaires);
+    questionnaires.value = data.questionnaires.map(questionnaire => ({ id: questionnaire.id, name: questionnaire.name }));
+  } catch (error) {
+    console.error("Erreur lors de la récupération des questionnaires:", error);
+  }
 });
 
-const removeItem = (id) => {
-  tasks.value = tasks.value.filter(task => task.id !== id.id);
+const removeItem = (idToRemove) => {
+  questionnaires.value = questionnaires.value.filter(questionnaire => questionnaire.id !== idToRemove);
 };
 
-const modifierItem = (id) => {
-  for (let i = 0; i < tasks.value.length; i++) {
-    if (tasks.value[i].id === id.id) {
-      tasks.value[i].title = prompt('Entrez le nouveau titre de la tâche');
-      break;
+const modifierItem = (idToModify) => {
+  const questionnaireToModify = questionnaires.value.find(questionnaire => questionnaire.id === idToModify);
+  if (questionnaireToModify) {
+    const newName = prompt('Entrez le nouveau titre du questionnaire', questionnaireToModify.name);
+    if (newName !== null) {
+      questionnaireToModify.name = newName;
     }
   }
 };
+
+const afficherDetail = (idToDetail) => {
+  const questionnaireToDisplayDetail = questionnaires.value.find(questionnaire => questionnaire.id === idToDetail.id);
+  fetch('http://127.0.0.1:5000/questionnaires/'+idToDetail.id+'/questions')
+  .then(response => response.json())
+  .then(data => {
+    var affichage = "<h3>"+idToDetail.id+" "+questionnaireToDisplayDetail.name+"</h3>";
+    data.questions.forEach(question => {
+      affichage += (question.answers ?? question.answer);
+    });
+    // Sélectionnez l'élément HTML où vous voulez afficher les détails
+    const detailElement = document.querySelector('.detail');
+    // Mettez à jour son contenu avec les détails
+    detailElement.innerHTML = affichage;
+  });
+};
+
+
+
 </script>
 
 <style scoped>
 .app {
-  text-align: center;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
 }
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+
 </style>
